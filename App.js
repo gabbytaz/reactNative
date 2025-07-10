c1131cd4135b747416479b8c050aedf594f3f6fe
gabbytaz <gabrielaeras@hotmail.com>
2025-07-10 17:46:02 -0500

    trabajo en clase

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Button, ScrollView } from 'react-native-web';
+import { getPersonajes } from './lib/rickyMorty';
+import Logo from './components/Logo';
 
 export default function App() {
+  const [personajes, setPersonajes] = useState([]);
+
+  useEffect(() => {
+    console.log("Fetching ");
+    getPersonajes().then((data) => {
+      setPersonajes(data);
+      console.log("personajes:", data);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Logo style={styles.logo}></Logo>
+        {personajes.map((character) => (
+          <View style={styles.card} key={character.id}>
+            <Image style={styles.image} source={{ uri: character.image }}></Image>
+            <Text>{character.id}</Text>
+            <Text style={styles.title}>{character.name}</Text>
+            <Text style={styles.species}>{character.species}</Text>
+            <Text style={styles.status}>{character.status}</Text>
+            <Text style={styles.gender}>{character.gender}</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
@@ -17,4 +43,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+  card: {
+    backgroundColor: '#333',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+    width: '90%',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#fff'
+  },
+  species: {
+    fontSize: 16,
+    color: '#fff'
+  },
+  status: {
+    fontSize: 16,
+    color: '#33caff'
+  },
+  gender: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  logo: {
+    backgroundColor: '#aaa',
+    padding: 20
+  }
 });
+
